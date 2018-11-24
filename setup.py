790bf08bcf057f4543d9e47936c25ed00ba6d1b6
Steven Hazel <sah@saucelabs.com>
2018-11-24 10:36:18 -0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from monocle import VERSION
+
+install_requires = []
+
+setup(name="tricle",
+      version=VERSION,
+      description="An async programming framework with a blocking look-alike syntax",
+      author="Greg Hazel and Steven Hazel",
+      author_email="sah@awesame.org",
+      maintainer="Steven Hazel",
+      maintainer_email="sah@awesame.org",
+      url="http://github.com/saucelabs/tricle",
+      packages=['monocle',
+                'monocle.stack',
+                'monocle.stack.network',
+                'monocle.twisted_stack'],
+      install_requires=install_requires,
+      license='MIT'
+      )
