fa348a21c6bd33d801af958ad1c518281b0c2ffb
Steven Hazel <sah@saucelabs.com>
2019-02-26 16:08:16 -0800

    Now that Callbacks are awaitable via their underlying Future, they can wind up having it cancelled. We can't cancel the operations, alas, but no need to freak out if the result is no longer wanted.

--- monocle/callback.py
+++ monocle/callback.py
@@ -2,11 +2,6 @@ import sys
 import asyncio
 import asyncio.base_futures
 import collections
-import logging
-
-logging.basicConfig(stream=sys.stderr,
-                    format="%(message)s")
-log = logging.getLogger("monocle")
 
 
 class Callback(collections.abc.Awaitable):
@@ -32,25 +27,11 @@ class Callback(collections.abc.Awaitable):
     def __call__(self, result):
         assert not hasattr(self, 'result'), "Already called back"
         self.result = result
-        try:
+        if not self.future.cancelled():
             if isinstance(result, BaseException):
                 self.future.set_exception(result)
             else:
                 self.future.set_result(result)
-        except asyncio.base_futures.InvalidStateError:
-            # this should never happen, because we checked that we
-            # haven't been called back, above. But, it does, so log
-            # some info about it. Gotta love this future API:
-            r = None
-            e = None
-            c = self.future.cancelled()
-            if not c:
-                e = self.future.exception()
-                if not e:
-                    r = self.future.result()
-            log.exception("future already called back in Callback! future result: c: %s, e: %s, r: %s; cb result: %s",
-                          c, e, r, self.result)
-            raise
 
     def __await__(self):
         return self.future.__await__()
